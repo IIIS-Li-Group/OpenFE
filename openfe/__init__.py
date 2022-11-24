5bed602c61a69826c46bc0c32e0182a6991d5df0
ztphs <ztp18@mails.tsinghua.edu.cn>
2022-11-24 10:12:21 +0800

    update __init__ for sphinx

--- openfe/__init__.py
+++ openfe/__init__.py
@@ -3,6 +3,12 @@
 # License: MIT
 
 name = "OpenFE"
-__version__ = "0.0.1"
+__version__ = "0.0.3"
 from .openfe import openfe, get_candidate_features
 from .utils import transform, tree_to_formula, formula_to_tree
+
+# __all__ = ['openfe', 'get_candidate_features', 'transform']
+__all__ = []
+for v in dir():
+    if not v.startswith('__'):
+        __all__.append(v)
